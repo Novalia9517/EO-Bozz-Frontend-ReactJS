36fbf05a706ba7f78963fe90e026d0dbcd5930e7
Novalia9517 <novalia.9517@gmail.com>
2022-12-22 16:19:17 +0700

    Login clients, partner, admin

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -5,7 +5,16 @@ module.exports = {
     "./src/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        'bozz-one': '#533e85',
+        'bozz-two': '#488fb1',
+        'bozz-three': '#4fd3c4',
+        'bozz-four': '#c1f8cf',
+        'bozz-five':'#E5E5E5',
+        'bozz-six':'#F9F5F6'
+      },
+    },
   },
   plugins: [
     require("daisyui")
