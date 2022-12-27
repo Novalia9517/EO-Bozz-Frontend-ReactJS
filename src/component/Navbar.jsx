5c1557db94b74c6c69c1288070fe5699c5c320e7
gilangsup <gilangsprb@gmail.com>
2022-12-28 02:37:13 +0700

    change cookie to localstorage

--- src/component/Navbar.jsx
+++ src/component/Navbar.jsx
@@ -1,8 +1,42 @@
 import React from 'react'
-import { Link } from 'react-router-dom'
-
+import { Link, useNavigate } from 'react-router-dom'
+import Swal from 'sweetalert2'
+import { useCookies } from 'react-cookie'
 
 const Navbar = () => {
+
+    const [cookie, removeCookie] = useCookies(["userToken"])
+    const navigate = useNavigate()
+    const token = localStorage.getItem("userToken")
+
+    const onLogout = () => {
+        Swal.fire({
+            title: "Are you sure?",
+            icon: "warning",
+            showCancelButton: true,
+            confirmButtonColor: "#17345f",
+            confirmButtonText: "Yes, sure",
+            cancelButtonColor: "#F47522",
+            cancelButtonText: "Not now",
+        }).then((result) => {
+            if (result.isConfirmed) {
+                Swal.fire({
+                    position: "center",
+                    icon: "success",
+                    text: "logout success, see you 👋🏻",
+                    showConfirmButton: false,
+                    timer: 1500,
+                });
+                localStorage.removeItem("userToken");
+                localStorage.removeItem("name");
+                localStorage.removeItem("id");
+                localStorage.removeItem("role");
+
+                navigate('/login')
+            }
+        });
+    }
+
     return (
         <div className='relative bg-bozz-one'>
             <div className='container mx-auto py-px-10'>
@@ -24,14 +58,20 @@ const Navbar = () => {
                                     <img src="https://placeimg.com/80/80/people" />
                                 </div>
                             </label>
-                            <ul tabIndex={0} className="text-bozz-one mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
-                                <li>
-                                    <Link to={"/profile"} className="justify-between">
-                                        Profile
-                                    </Link>
-                                </li>
-                                <li><a >Logout</a></li>
-                            </ul>
+                            {token ? (
+                                <ul tabIndex={0} className="text-bozz-one mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52">
+                                    <li>
+                                        <Link to={"/profile"} className="justify-between">
+                                            Profile
+                                        </Link>
+                                    </li>
+                                    <li className='cursor-pointer' onClick={onLogout}>Logout</li>
+                                </ul>
+                            ) : <div>
+                                <button>Login</button>
+                                <button>Register</button>
+                            </div>}
+
                         </div>
                     </div>
                 </div>
--- src/pages/Partner/dashboard.jsx
+++ src/pages/Partner/dashboard.jsx
@@ -3,36 +3,46 @@ import { Link, useNavigate } from 'react-router-dom'
 import LayoutAdmin from '../../components/LayoutAdmin'
 import { FaEdit, FaTrashAlt } from 'react-icons/fa'
 import { formatCurrency } from '../../utils/formatCurrency'
+import { useCookies } from 'react-cookie';
+import axios from 'axios'
 
 const Dashboard = () => {
     const [status, setStatus] = useState()
     const [logs, setLogs] = useState(['register', 'waiting for verify', 'revision with note'])
     const [listServices, setListServices] = useState('')
     const [listAdditional, setListAdditional] = useState('')
-    const serviceHead = ['no','package name', 'package price', 'package category', 'action']
-    const additionalHead =  ['no','additional name', 'additional price']
+    const serviceHead = ['no', 'package name', 'package price', 'package category', 'action']
+    const additionalHead = ['no', 'additional name', 'additional price']
     const [active, setActive] = useState('service')
+    const [cookie, setCookie] = useCookies()
+
+    const getDataPartner = async () => {
+        await axios.get(`https://irisminty.my.id/${cookie.id}`, {
+            headers: { Authorization: `Bearer ${cookie.token}` },
+        })
+    }
+
 
     const navigate = useNavigate()
     const getListServices = () => {
         const services = [
             {
-                id : 1,
-                package_name : 'Wedding Package',
-                package_price : 12000000,
-                package_category : 'wedding',
+                id: 1,
+                package_name: 'Wedding Package',
+                package_price: 12000000,
+                package_category: 'wedding',
             },
             {
-                id :2,
-                package_name : 'Wedding Package',
-                package_price : 12000000,
-                package_category : 'wedding',
+                id: 2,
+                package_name: 'Wedding Package',
+                package_price: 12000000,
+                package_category: 'wedding',
             },
             {
                 id: 3,
-                package_name : 'Wedding Package',
-                package_price : 12000000,
-                package_category : 'wedding',
+                package_name: 'Wedding Package',
+                package_price: 12000000,
+                package_category: 'wedding',
             },
         ]
 
@@ -41,16 +51,16 @@ const Dashboard = () => {
     const getListAdditionals = () => {
         const additionals = [
             {
-                additional_name : 'Wedding additional',
-                additional_price : 12000000,
+                additional_name: 'Wedding additional',
+                additional_price: 12000000,
             },
             {
-                additional_name : 'Wedding additional',
-                additional_price : 12000000,
+                additional_name: 'Wedding additional',
+                additional_price: 12000000,
             },
             {
-                additional_name : 'Wedding additional',
-                additional_price : 12000000,
+                additional_name: 'Wedding additional',
+                additional_price: 12000000,
             },
         ]
 
@@ -58,110 +68,111 @@ const Dashboard = () => {
     }
     const goEdit = (id) => {
         navigate('/partner/edit-service', {
-          state : { id : id}
+            state: { id: id }
         })
-      }
+    }
 
     useEffect(() => {
+        getDataPartner()
         setStatus('verify')
         getListServices()
         getListAdditionals()
-    },[])
-  return (
-    <LayoutAdmin>
-        {status === 'not verify' &&
-            <div>
-                <h1 className='text-xl font-bold text-bozz-one mb-5 border border-bozz-one p-5 rounded-xl'>Account Not Verify Yet</h1>
-                <h1 className='text-xl font-bold text-bozz-one'>Verify Progress</h1>
-                <ul className="steps steps-vertical">
-                    {logs?.map((log, index) => {
-                        return  <li className="step step-primary text-bozz-one capitalize">
-                            <span>{log}
-                            {log === 'revision with note' ? <a className='text-bozz-two font-bold underline'> Link Revisi</a>: ''}
-                            </span>
+    }, [])
+    return (
+        <LayoutAdmin>
+            {status === 'not verify' &&
+                <div>
+                    <h1 className='text-xl font-bold text-bozz-one mb-5 border border-bozz-one p-5 rounded-xl'>Account Not Verify Yet</h1>
+                    <h1 className='text-xl font-bold text-bozz-one'>Verify Progress</h1>
+                    <ul className="steps steps-vertical">
+                        {logs?.map((log, index) => {
+                            return <li className="step step-primary text-bozz-one capitalize">
+                                <span>{log}
+                                    {log === 'revision with note' ? <a className='text-bozz-two font-bold underline'> Link Revisi</a> : ''}
+                                </span>
                             </li>
-                    })}
-                </ul>
-            </div>
-        }
-        {status === 'verify' && listServices === '' ? 
-            <div className='flex flex-col mt-3'>
-                <button className='bg-bozz-two text-bozz-six h-8 py-0 rounded-lg mb-5 self-end' onClick={() => navigate('/partner/add-service')}> Add New Service</button>
-                <h1 className='text-xl font-bold text-bozz-one mb-5 border border-bozz-one p-5 rounded-xl'>List Services Not Add Yet</h1>
-            </div> 
-        : null
-        }
-        {status === 'verify' && listServices !== '' ? 
-            <div className='flex flex-col mt-3'>
-                <button className='bg-bozz-two text-bozz-six h-8 py-0 px-8 rounded-lg mb-5 self-end' onClick={() => navigate('/partner/add-service')}> Add New Service</button>
-                <div className='flex'>
-                    <div 
-                        className={`w-40 text-sm h-8 text-center py-2 ${active == 'service' ? `bg-bozz-two text-white font-semibold` : `bg-bozz-six border border-bozz-two text-bozz-two font-semibold`}`}
-                        onClick={() => setActive('service')}>List Services</div>
-                    <div 
-                        className={`w-40 text-sm h-8  text-center py-2 ${active != 'service' ? `bg-bozz-two  text-white font-semibold` : `bg-bozz-six border border-bozz-two text-bozz-two font-semibold`}`}
-                        onClick={() => setActive('additional')}
-                        >List Additionals</div>
+                        })}
+                    </ul>
                 </div>
-                {active == 'service'
-                    ?  
+            }
+            {status === 'verify' && listServices === '' ?
+                <div className='flex flex-col mt-3'>
+                    <button className='bg-bozz-two text-bozz-six h-8 py-0 rounded-lg mb-5 self-end' onClick={() => navigate('/partner/add-service')}> Add New Service</button>
+                    <h1 className='text-xl font-bold text-bozz-one mb-5 border border-bozz-one p-5 rounded-xl'>List Services Not Add Yet</h1>
+                </div>
+                : null
+            }
+            {status === 'verify' && listServices !== '' ?
+                <div className='flex flex-col mt-3'>
+                    <button className='bg-bozz-two text-bozz-six h-8 py-0 px-8 rounded-lg mb-5 self-end' onClick={() => navigate('/partner/add-service')}> Add New Service</button>
+                    <div className='flex'>
+                        <div
+                            className={`w-40 text-sm h-8 text-center py-2 ${active == 'service' ? `bg-bozz-two text-white font-semibold` : `bg-bozz-six border border-bozz-two text-bozz-two font-semibold`}`}
+                            onClick={() => setActive('service')}>List Services</div>
+                        <div
+                            className={`w-40 text-sm h-8  text-center py-2 ${active != 'service' ? `bg-bozz-two  text-white font-semibold` : `bg-bozz-six border border-bozz-two text-bozz-two font-semibold`}`}
+                            onClick={() => setActive('additional')}
+                        >List Additionals</div>
+                    </div>
+                    {active == 'service'
+                        ?
                         <div className='p-5 bg-white rounded-lg'>
                             <table className='w-full table-auto'>
-                                    <thead className='border-b-2 border-bozz-one'>
-                                        <tr>
-                                            {serviceHead?.map((title,index) => {
-                                                return <td className='text-bozz-three font-semibold capitalize' key={index}>{title}</td>
-                                            })}
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                    {listServices.map((data, index) => {
-                                        return (
-                                        <tr className='text-bozz-three border-b-2 border-bozz-three h-12 text-center py-10' key={index}>
-                                            <td>{index + 1}</td>
-                                            <td>{data.package_name}</td>
-                                            <td>{formatCurrency(data.package_price)}</td>
-                                            <td>{data.package_category}</td>
-                                            <td className='flex justify-evenly items-center py-3 text-lg'>
-                                                <FaEdit className='text-bozz-two' onClick={() => goEdit(data.id)}/>
-                                                <FaTrashAlt className='text-red-400'/>
-                                            </td>
-                                        </tr>
-                                        )
-                                    })}
-                                    </tbody>
-                                </table>
-                        </div>
-                    : <div className='p-5 bg-white rounded-lg'>
-                        <table className='w-full table-auto'>
-                                <thead className='border-b-2 border-bozz-three'>
-                                {additionalHead?.map((title,index) => {
-                                    return <th className='text-bozz-three font-semibold capitalize' key={index}>{title}</th>
-                                })}
+                                <thead className='border-b-2 border-bozz-one'>
+                                    <tr>
+                                        {serviceHead?.map((title, index) => {
+                                            return <td className='text-bozz-three font-semibold capitalize' key={index}>{title}</td>
+                                        })}
+                                    </tr>
                                 </thead>
                                 <tbody>
-                                {listAdditional?.map((data, index) => {
-                                    return (
-                                    <tr className='text-bozz-three border-b-2 border-bozz-three h-10 text-center' key={index}>
-                                        <td>{index + 1}</td>
-                                        <td>{data.additional_name}</td>
-                                        <td>{formatCurrency(data.additional_price)}</td>
-                                        <td  className='flex justify-evenly items-center py-3 text-lg'>
-                                            <FaEdit className='text-bozz-two'/>
-                                            <FaTrashAlt className='text-red-400'/>
-                                        </td>
-                                    </tr>
-                                    )
-                                })}
+                                    {listServices.map((data, index) => {
+                                        return (
+                                            <tr className='text-bozz-three border-b-2 border-bozz-three h-12 text-center py-10' key={index}>
+                                                <td>{index + 1}</td>
+                                                <td>{data.package_name}</td>
+                                                <td>{formatCurrency(data.package_price)}</td>
+                                                <td>{data.package_category}</td>
+                                                <td className='flex justify-evenly items-center py-3 text-lg'>
+                                                    <FaEdit className='text-bozz-two' onClick={() => goEdit(data.id)} />
+                                                    <FaTrashAlt className='text-red-400' />
+                                                </td>
+                                            </tr>
+                                        )
+                                    })}
+                                </tbody>
+                            </table>
+                        </div>
+                        : <div className='p-5 bg-white rounded-lg'>
+                            <table className='w-full table-auto'>
+                                <thead className='border-b-2 border-bozz-three'>
+                                    {additionalHead?.map((title, index) => {
+                                        return <th className='text-bozz-three font-semibold capitalize' key={index}>{title}</th>
+                                    })}
+                                </thead>
+                                <tbody>
+                                    {listAdditional?.map((data, index) => {
+                                        return (
+                                            <tr className='text-bozz-three border-b-2 border-bozz-three h-10 text-center' key={index}>
+                                                <td>{index + 1}</td>
+                                                <td>{data.additional_name}</td>
+                                                <td>{formatCurrency(data.additional_price)}</td>
+                                                <td className='flex justify-evenly items-center py-3 text-lg'>
+                                                    <FaEdit className='text-bozz-two' />
+                                                    <FaTrashAlt className='text-red-400' />
+                                                </td>
+                                            </tr>
+                                        )
+                                    })}
                                 </tbody>
                             </table>
                         </div>
 
-                }
-            </div> : null
-        }
-    </LayoutAdmin>
-  )
+                    }
+                </div> : null
+            }
+        </LayoutAdmin>
+    )
 }
 
 export default Dashboard
